8dc9edd47f2c364696852a4ba703cd920a47eacf
phillipivan <phillip.pietruschka@parliament.vic.gov.au>
2024-07-04 11:10:33 +1000

    formatting

--- src/sequence.js
+++ src/sequence.js
@@ -1,8 +1,8 @@
 export default function (self) {
-    if (self.mvp.sequence === undefined) {
-        self.mvp.sequence = 1 // sequence number 0 reserved
-    }
-    let sequence = self.mvp.sequence.toString(36).padStart(4,'0')
-    self.mvp.sequence = self.mvp.sequence >= 1679615 ? 0 : self.mvp.sequence + 1
-    return sequence
+	if (self.mvp.sequence === undefined) {
+		self.mvp.sequence = 1 // sequence number 0 reserved
+	}
+	let sequence = self.mvp.sequence.toString(36).padStart(4, '0')
+	self.mvp.sequence = self.mvp.sequence >= 1679615 ? 0 : self.mvp.sequence + 1
+	return sequence
 }
